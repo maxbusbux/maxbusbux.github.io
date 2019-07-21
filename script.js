2fa807c316af7584eca028433c541793ccd796f5
maxbusbux <max.gnedenko06@gmail.com>
2019-07-21 20:57:46 +0300

    Moved to materialize.css and added reset button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-
 function run() {
   indata = document.getElementById("vin").value;
   outdata = btoa(indata);
   document.getElementById("vout").setAttribute('value', outdata);
-}
+};
 
 function press() {
   run();
 }
+function reset(){
+$('#vin').val('').removeClass('valid');
+M.updateTextFields();
+$('#vout').val('').removeClass('valid');
+M.updateTextFields();
+};
