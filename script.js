51d287167935be1ed6d72dcada33f544dc1632bb
maxbusbux <max.gnedenko06@gmail.com>
2019-07-28 13:42:08 +0300

    Fixed icon

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ function run() {
   indata = document.getElementById("vin").value;
   outdata = btoa(indata);
   document.getElementById("vout").setAttribute('value', outdata);
+  if (indata == null || indata == "") {
+    document.getElementById('vin').className="disabled"
+
+  }
+
 };
 
 function press() {
   run();
 }
-function reset(){
-$('#vin').val('').removeClass('valid');
-M.updateTextFields();
-$('#vout').val('').removeClass('valid');
-M.updateTextFields();
+
+function reset() {
+  $('#vin').val('').removeClass('valid');
+  M.updateTextFields();
+  $('#vout').val('').removeClass('valid');
+  M.updateTextFields();
 };
