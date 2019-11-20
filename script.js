c6a83a7485ef456e8e8948d91079ef0654ceb90b
maxbusbux <max.gnedenko06@gmail.com>
2019-11-20 20:44:43 +0300

    Added Decode Feature

--- script.js
+++ script.js
@@ -3,19 +3,36 @@ function run() {
   outdata = btoa(indata);
   document.getElementById("vout").setAttribute('value', outdata);
   if (indata == null || indata == "") {
-    document.getElementById('vin').className="disabled"
+    document.getElementById('vin').className = "disabled"
 
   }
 
 };
 
+function run1() {
+  indata1 = document.getElementById("vin1").value;
+  outdata1 = atob(indata1);
+  document.getElementById("vout2").setAttribute('value', outdata1);
+};
+
 function press() {
   run();
+};
+
+function press1() {
+  run1();
 }
 
 function reset() {
   $('#vin').val('').removeClass('valid');
   M.updateTextFields();
   $('#vout').val('').removeClass('valid');
+  M.updateTextFields();
+};
+
+function reset1() {
+  $('#vin1').val('').removeClass('valid');
+  M.updateTextFields();
+  $('#vout2').val('').removeClass('valid');
   M.updateTextFields();
 };
