5629d15ee44980623bc0e83884ce92d85383dd94
maxbusbux <max.gnedenko06@gmail.com>
2020-05-29 12:13:27 +0300

    Added menu and cleaned files

---
+++ script1.js
@@ -0,0 +1,8 @@
+function hash_text(){
+indata = document.getElementById("input").value;
+hash = calcMD5(indata);
+document.getElementById("output").setAttribute('value', hash);
+};
+function press(){
+hash_text();
+};
